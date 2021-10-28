f3cb141f6e3471107fe2f1be293f42090bb76157
RAGUL VIGNESH M <92584952+RagulVignesh@users.noreply.github.com>
2021-10-28 07:28:50 +0530

    Update funarg.sh

--- funarg.sh
+++ funarg.sh
@@ -2,12 +2,22 @@
 
 hi()
 {
+echo "hi $1, THis is my first live"  #$1 is the argument taken for consideration from function call based on its position
+}
+hi Ragul                             #Ragul is the argument which is passed to the functions
+
+
+
+
+#Script for functions with return type
+
+hi()
+{
 echo "hi $1, THis is my first live"
-return 100
+return 100                          # Return statement is used to pass the return value
 }
 
 hi Ragul
-ret=$?
-
+ret=$?                              # The values passed by the functions have to be captured right after the function call
 echo "THe return value is $ret"
-hi Prathik
+
